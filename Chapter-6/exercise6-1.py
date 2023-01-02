ed4eb0bc73ca0e3bed55c24830d15909b8fc4b62
Tahmid Kazi <101431441+tahmid-kazi@users.noreply.github.com>
2023-01-02 15:46:34 -0500

    Update exercise6-1.py

--- Chapter-6/exercise6-1.py
+++ Chapter-6/exercise6-1.py
@@ -2,7 +2,8 @@ dict_json = {
     "first_name":"Lex",
     "last_name":"Fridman",
     "city":"Austin",
-    "age":39
+    "age":39,
+    "country":"USA"
     }
 print(dict_json['age'])
 #print(dict_json)
